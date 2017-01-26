e55651bb0103fc6116f4c8398e415cae810754b7
merxer <34d30y@gmail.com>
2017-01-26 16:08:24 +0700

    receive parameter from post method

--- src/api.go
+++ src/api.go
@@ -30,6 +30,14 @@ func get_users_id(c echo.Context) error {
 	return c.String(http.StatusOK, id)
 }
 
+func update_user(c echo.Context) error {
+	user := new(User)
+	if err := c.Bind(user); err != nil {
+		return c.JSON(http.StatusBadRequest, nil)
+	}
+	return c.JSON(http.StatusOK, user)
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -37,6 +45,7 @@ func main() {
 	e.GET("/", index)
 	e.GET("/users", get_users)
 	e.GET("/users/:id", get_users_id)
+	e.POST("/users", update_user)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
