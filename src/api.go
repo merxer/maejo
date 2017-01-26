317a8c3381834e456ba7fcb7ae71054a1defad44
merxer <34d30y@gmail.com>
2017-01-26 15:15:30 +0700

    result from struct and convert to JSON object

--- src/api.go
+++ src/api.go
@@ -7,13 +7,21 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+type User struct {
+	Firstname string
+	Lastname string
+}
+
 func index(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
 func get_users(c echo.Context) error {
-	firstname := "thawatchai"
-	return c.JSON(http.StatusOK, firstname)
+	pat := User{
+		"thawatchai",
+		"singngam",
+	}
+	return c.JSON(http.StatusOK, pat)
 }
 
 func main() {
