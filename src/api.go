955d6b0e69cf07fd044969016fb286615f73bc14
merxer <34d30y@gmail.com>
2017-01-26 15:42:15 +0700

    send and receive parameter on api

--- src/api.go
+++ src/api.go
@@ -25,12 +25,18 @@ func get_users(c echo.Context) error {
 	return c.JSON(http.StatusOK, pat)
 }
 
+func get_users_id(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
 	e.GET("/", index)
 	e.GET("/users", get_users)
+	e.GET("/users/:id", get_users_id)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
